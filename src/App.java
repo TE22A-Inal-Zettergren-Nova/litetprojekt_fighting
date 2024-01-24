3bfda8bfa3f29ffb686d47ffdb6bc5d818e1320e
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2024-01-24 10:35:17 +0100

    klar med meny, startar, avslutar och restartar

--- src/App.java
+++ src/App.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class App {
+
+            
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner tb = new Scanner(System.in);
+        System.out.println("Meny \n 1. Starta \n 2. Avsluta");
+        int startval = tb.nextInt();
+
+        while (startval!=3) {
+            if (startval==1)
+            {
+                System.out.println("Spelet startar...");
+                break;
+            }
+
+            else if(startval==2)
+            {
+                System.out.println("Spelet avslutas...");
+                System.exit(0);
+                break;
+            }
+
+            else{
+                System.out.println("Detta val finns inte");
+                main(args);
+                break;
+            }
+        }
+        
+          
+        }
     }
-}
+
+    
+
