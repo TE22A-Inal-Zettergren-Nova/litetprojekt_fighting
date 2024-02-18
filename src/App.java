38654a0529a91f72a89f5001ecdd34cad829e75e
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2024-02-18 17:32:28 +0100

    lade till färger och små ändringar i koden

--- src/App.java
+++ src/App.java
@@ -14,6 +14,8 @@ public class App {
     public static String BLUE = "\u001B[34m";
     public static String YELLOW = "\u001B[33m";
     public static String CYAN = "\u001B[36m";
+    public static String PURPLE = "\u001B[35m";
+
     
 
     //metod för när spelaren attackerar//
@@ -33,49 +35,54 @@ public class App {
         int chansA5 = 20;
         int träffA5 = rand.nextInt(chansA5);
  
-        System.out.println(CYAN+"Välj din attack: \n 1. Poke \n 2. Punch \n 3. Kick \n 4. Stab \n 5. Sword slash\n"+CYAN);
+        System.out.println(GREEN+"\nVälj din attack: \n 1. Poke \n 2. Punch \n 3. Kick \n 4. Stab \n 5. Sword slash\n"+RESET);
         int val = tb.nextInt(); 
 
             switch (val) {
                 case 1:
-                    System.out.println("Du gjorde 5 skada");
-                    System.out.println("Motståndaren har nu: " + (motståndareHP= motståndareHP -5) + "HP");
+                    System.out.println(RED+"Du träffa motståndaren och gjorde 5 skada"+RESET);
+                    System.out.println(PURPLE+"Motståndaren har nu: " + (motståndareHP= motståndareHP -5) + "HP\n");
                     break;
                 case 2:
                     if (träffA2 == 2) {
-                        System.out.println("Du missade \nMotståndaren har fortfarande: " + motståndareHP +"HP");
+                        System.out.println(PURPLE+"Du missade \nMotståndaren har fortfarande: " + motståndareHP +"HP\n"+RESET);
                     }
                     else{
-                        System.out.println("Du gjorde 15 skada \nMotståndaren har nu: " + (motståndareHP=motståndareHP - 15) + "HP");
-               
+                        System.out.println(RED+"Du träffa motståndaren och  gjorde 15 skada"+RESET);
+                        System.out.println(PURPLE+"Motståndaren har nu: " + (motståndareHP=motståndareHP - 15) + "HP\n"+RESET);
                      }
                     break;
                 case 3:
                     if (träffA3 == 1) {
-                        System.out.println("Du missade \nMotståndaren har fortfarande: " + motståndareHP +"HP");
+                        System.out.println(PURPLE+"Du missade \nMotståndaren har fortfarande: " + motståndareHP +"HP\n"+RESET);
                     }   
                     else{
-                        System.out.println("Du gjorde 25 skada \nMotståndaren har nu: " + (motståndareHP=motståndareHP- 25) + "HP");
+                        System.out.println(RED+"Du träffa motståndaren och  gjorde 25 skada"+RESET);
+                        System.out.println(PURPLE+"Motståndaren har nu: " + (motståndareHP=motståndareHP - 25) + "HP\n"+RESET);
+
                     } 
                     break;
                 case 4:
                     if (träffA4 == 1) {
-                        System.out.println("Du missade \nMotståndaren har fortfarande: " + motståndareHP +"HP");
+                        System.out.println(PURPLE+"Du missade \nMotståndaren har fortfarande: " + motståndareHP +"HP\n"+RESET);
                     }
                     else{
-                        System.out.println("Du gjorde 50 skada \nMotståndaren har nu: " + (motståndareHP=motståndareHP - 50) + "HP");
+                        System.out.println(RED+"Du träffa motståndaren och  gjorde 50 skada "+RESET);
+                        System.out.println(PURPLE+"Motståndaren har nu: " + (motståndareHP=motståndareHP - 50) + "HP\n"+RESET);
                     }
                     break;
                 case 5:
                     if (träffA5 == 20) {
-                        System.out.println("Du gjorde 100 skada \nMotståndaren har nu: " + (motståndareHP=motståndareHP - 100) + "HP");
+                        System.out.println(RED+"Du träffa motståndaren och gjorde 100 skada"+RESET);
+                        System.out.println(PURPLE+"Motståndaren har nu: " + (motståndareHP=motståndareHP - 100) + "HP\n"+RESET);
+
                     }
                     else{
-                        System.out.println("Du missade \nMotståndaren har fortfarande: " + motståndareHP +"HP");
+                        System.out.println(PURPLE+"Du missade \nMotståndaren har fortfarande: " + motståndareHP +"HP\n"+RESET);
                     } 
                     break;
                 default:
-                    System.out.println("Detta val finns inte välj något av ovanstående");
+                    System.out.println(GREEN+"Detta val finns inte välj något av ovanstående\n"+RESET);
                     break;
                 }
             }
@@ -101,39 +108,45 @@ public class App {
 
             switch (motståndareVAL) {
                 case 0:
-                    System.out.println("Motståndaren gjorde 5 skada");
-                    System.out.println("Du har nu: " + (spelareHP= spelareHP -5) + "HP");
+                    System.out.println(RED+"Du blev träffad, motståndaren gjorde 5 skada"+RESET);
+                    System.out.println(CYAN+"Du har nu: " + (spelareHP= spelareHP -5) + "HP"+RESET);
                     break;
                 case 1:
                     if (träffA2 == 2) {
-                        System.out.println("Motståndaren missade \nDu har fortfarande: " + spelareHP +"HP");
+                        System.out.println(CYAN+"Motståndaren missade \nDu har fortfarande: " + spelareHP +"HP"+RESET);
                     }
                     else{
-                        System.out.println("Motståndaren gjorde 15 skada \nDu har nu: " + (spelareHP=spelareHP - 15) + "HP");
+                        System.out.println(RED+"Du blev träffad, motståndaren gjorde 15 skada"+RESET);
+                        System.out.println(CYAN+"Du har nu: " + (spelareHP=spelareHP - 15) + "HP"+RESET);
                      }
                     break;
                 case 2:
                     if (träffA3 == 1) {
-                        System.out.println("Motståndaren missade \nDu har fortfarande: " + spelareHP +"HP");
+                        System.out.println(CYAN+"Motståndaren missade \nDu har fortfarande: " + spelareHP +"HP"+RESET);
                     }   
                     else{
-                        System.out.println("Motståndaren gjorde 25 skada \nDu har nu: " + (spelareHP=spelareHP- 25) + "HP");
+                        System.out.println(RED+"Du blev träffad, motståndaren gjorde 25 skada"+RESET);
+                        System.out.println(CYAN+"Du har nu: " + (spelareHP=spelareHP - 25) + "HP"+RESET);
+
                     } 
                     break;
                 case 3:
                     if (träffA4 == 1) {
-                        System.out.println("Motståndaren missade \nDu har fortfarande: " + spelareHP +"HP");
+                        System.out.println(RED+"Du blev träffad, motståndaren gjorde 50 skada"+RESET);
+                        System.out.println(CYAN+"Du har nu: " + (spelareHP=spelareHP - 50) + "HP"+RESET);
                     }
                     else{
-                        System.out.println("Motståndaren gjorde 50 skada \nDu har nu: " + (spelareHP=spelareHP - 50) + "HP");
+                        System.out.println(CYAN+"Motståndaren missade \nDu har fortfarande: " + spelareHP +"HP"+RESET);
                     }
                     break;
                 case 4:
                     if (träffA5 == 20) {
-                        System.out.println("Motståndaren gjorde 100 skada \nDu har nu: " + (spelareHP=spelareHP - 100) + "HP");
+                        System.out.println(RED+"Du blev träffad, motståndaren gjorde 100 skada"+RESET);
+                        System.out.println(CYAN+"Du har nu: " + (spelareHP=spelareHP - 100) + "HP"+RESET);
+
                     }
                     else{
-                        System.out.println("Motståndaren missade \nDu har fortfarande: " + spelareHP +"HP");
+                        System.out.println(CYAN+"Motståndaren missade \nDu har fortfarande: " + spelareHP +"HP"+RESET);
                     } 
                     break;
                 }
@@ -142,18 +155,18 @@ public class App {
     static void fem_HP_spelare(){
             
         Scanner tb = new Scanner(System.in);
-        System.out.println("Du har jävligt lågt HP, det är stor chans att du förlorar(sämst)");
-        System.out.println("Vad väljer du: \n 1. Hela dig med 25HP \n 2. Gör 30DMG på din motståndare");
+        System.out.println(PURPLE+"Du har jävligt lågt HP, det är stor chans att du förlorar(sämst)"+RESET);
+        System.out.println(PURPLE+"Vad väljer du: \n 1. Hela dig med 25HP \n 2. Gör 30DMG på din motståndare"+RESET);
         int HPval = tb.nextInt();
 
         if (HPval==1) {
-            System.out.println("Du hela dig med 25HP \n Du har nu: "+ (spelareHP=spelareHP+25)+"HP");
+            System.out.println(YELLOW+"Du hela dig med 25HP \n Du har nu: "+ (spelareHP=spelareHP+25)+"HP"+RESET);
         }
         else if (HPval==2) {
-            System.out.println("Du gjorde 30DMG \n Din motståndare har nu: "+(motståndareHP=motståndareHP-30)+"HP");
+            System.out.println(RED+"Du gjorde 30DMG \n Din motståndare har nu: "+(motståndareHP=motståndareHP-30)+"HP"+RESET);
         }
         else{
-            System.out.println("Detta val finns inte, försök igen");
+            System.out.println(GREEN+"Detta val finns inte, försök igen"+RESET);
             fem_HP_spelare();
         }
 
@@ -163,10 +176,10 @@ public class App {
         double HPval=Math.floor(Math.random() * 1);
 
         if (HPval==0) {
-            System.out.println("Motståndaren hela sig med 25HP \nMotståndaren har nu: "+ (motståndareHP=motståndareHP+25)+"HP");
+            System.out.println(YELLOW+"Motståndaren hela sig med 25HP \nMotståndaren har nu: "+ (motståndareHP=motståndareHP+25)+"HP"+RESET);
         }
         else if (HPval==1) {
-            System.out.println("Motståndaren gjorde 30DMG \nDu har nu: "+(spelareHP=spelareHP-30)+"HP");
+            System.out.println(RED+"Motståndaren gjorde 30DMG \nDu har nu: "+(spelareHP=spelareHP-30)+"HP"+RESET);
         }
 
     
@@ -203,22 +216,24 @@ public static void main(String[] args) throws Exception {
 
             Random rand = new Random(); 
             int motståndareTal = rand.nextInt(10); 
-            System.out.println(GREEN+"Ni ska nu avgöra vem som ska BÖRJA"+RESET);
-            System.out.println(GREEN+"Välj ett tal mellan 1-10"+RESET);
+            System.out.println(BLUE+"Ni ska nu avgöra vem som ska börja"+RESET);
+            System.out.println(BLUE+"Välj ett tal mellan 1-10"+RESET);
             int spelareTal = tb.nextInt();
-            System.out.println(GREEN+"Du väljer: "+spelareTal+RESET);
-            System.out.println(GREEN+"Motståndaren väljer: "+motståndareTal+RESET);
+            System.out.println(BLUE+"Du väljer: "+spelareTal+RESET);
+            System.out.println(BLUE+"Motståndaren väljer: "+motståndareTal+RESET);
+            Thread.sleep(1000);
         
             if (spelareTal>=motståndareTal) {
-                System.out.println("Ditt tal är större du börjar");
-                Thread.sleep(2500);
+                System.out.println(PURPLE+"Ditt tal är större du börjar"+RESET);
                 System.out.println(YELLOW+"\nInfo om attacker: \n Poke: 100% chans för träff, 5 DMG\n Punch: 80% chans för träff, 15 DMG\n Kick: 50% chans för träff, 25 DMG\n Stab: 20% chans för träff, 50 DMG\n Sword slash: 5% chans för träff, 100 DMG\n"+RESET); 
-            
+                Thread.sleep(2000);
+
                 while ((spelareHP >=0) && (motståndareHP >=0)){
-                    Thread.sleep(2000);
+                    Thread.sleep(1500);
                     spelareAttack();
                     if(motståndareHP<=0){break;}
                     else if (motståndareHP<=5 && motståndareHP>0) {fem_HP_motståndare();}
+                    Thread.sleep(1500);
                     motståndareAttack();
                     if (spelareHP<=5 && spelareHP>0) {fem_HP_spelare();}
                     }
@@ -231,6 +246,7 @@ public static void main(String[] args) throws Exception {
                     motståndareAttack();
                     if(motståndareHP<=0){break;}
                     else if (spelareHP<=5 && spelareHP>0) {fem_HP_spelare();}
+                    Thread.sleep(1500);
                     spelareAttack();
                     if (motståndareHP<=5 && motståndareHP>0) {fem_HP_motståndare();}
                     }
